94881bb3c530a498ab4eb43d8ea050da1d4eaf0d
Senyang Jiang <senyangjiang@yahoo.com>
2023-11-26 15:21:09 -0800

    different solvers

--- run.py
+++ run.py
@@ -4,7 +4,6 @@ import os
 import datetime
 import pandas as pd
 
-from common import *
 from benchmark import Benchmark
 
 def process(bm):
@@ -15,19 +14,19 @@ def main():
     ### Solver ###
     ap.add_argument("--solver_kind",
                     default="z3",
-                    choices=["z3"])
+                    choices=["z3", "kissat", "cadical"])
     ap.add_argument("--solver_args",
                     default="")
     ap.add_argument("--solver_bin",
                     default="z3",
-                    choices=["z3"])
+                    choices=["z3", "./kissat-3.1.0", "./cadical-1.9.0"])
     ### Benchmark ###
     ap.add_argument("--suite",
                     default="anni_2022",
                     choices=["anni_2022"])
     ap.add_argument("--count",
-                    type=int,
-                    default=None)
+                    default=None,
+                    type=int)
     ### Execution ###
     ap.add_argument("--parallel",
                     default=False,
@@ -37,7 +36,7 @@ def main():
                     type=int,
                     help="set global timeout, default is 60 seconds")
     ### Result ###
-    ap.add_argument("--input_csv",
+    ap.add_argument("--add_to_csv",
                     default=None)
     ap.add_argument("--report",
                     default=False,
@@ -67,8 +66,8 @@ def main():
 
     print("Read any previous benchmark results...")
     base = None
-    if options.input_csv and os.path.exists(options.input_csv):
-        base = pd.read_csv(options.input_csv)
+    if options.add_to_csv and os.path.exists(options.add_to_csv):
+        base = pd.read_csv(options.add_to_csv, index_col=0)
     else:
         base = pd.DataFrame(columns=["hash", "benchmark"])
         for bm in benchmarks:
@@ -78,7 +77,7 @@ def main():
 
     print("Perform benchmark...")
     solver_id = f"{options.solver_kind}+'{options.solver_args}'"
-    df = pd.DataFrame(columns=[f"time({solver_id})", f"result({solver_id})"])
+    df = pd.DataFrame(columns=[f"result({solver_id})", f"time({solver_id})"])
 
     start_time = datetime.datetime.now()
     n = 0
@@ -98,7 +97,7 @@ def main():
             result.add_csv(df)
 
     print(df)
-    print("Collect Results...")
+    print("Write Results...")
     out = pd.concat([base, df], axis=1)
     print(out)
     out.to_csv('output.csv', index=True)
